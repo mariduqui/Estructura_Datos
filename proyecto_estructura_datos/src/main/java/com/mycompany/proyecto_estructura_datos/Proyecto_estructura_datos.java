a71062781ed9409fae1382ff3447968a7358cdea
qmrn <qmrn@SJO-5CG12851XP.ant.amazon.com>
2023-10-31 00:56:24 -0600

    Se agregaron manejo de ordenes

--- proyecto_estructura_datos/src/main/java/com/mycompany/proyecto_estructura_datos/Proyecto_estructura_datos.java
+++ proyecto_estructura_datos/src/main/java/com/mycompany/proyecto_estructura_datos/Proyecto_estructura_datos.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.proyecto_estructura_datos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author ngranado
@@ -13,7 +16,11 @@ public class Proyecto_estructura_datos {
     public static void main(String[] args) {
         System.out.println("Proyecto Estructura de Datos Restaurante \n");
         System.out.println("Platillos \n");
+        
+        //Lista circular de platillos
         ListaCircular l = new ListaCircular();
+        
+        //Agrega platillos de forma estatica y muestra la informacion.
         
         l.inserta(new Platillo(0, "Arroz con Pollo", 3000, 4000));
         System.out.println(l.toString());
@@ -25,8 +32,41 @@ public class Proyecto_estructura_datos {
         System.out.println(l.toString());
         l.inserta(new Platillo(4, "Caviar", 7800, 10200));
         System.out.println(l.toString());
+        // Muestra el platillo que sigue despues del ultimo.
         System.out.println(l.imprimirSiguienteDelUltimo());
         
+        // Se crea una lista de tipo "LinkedList" para recrear la lista de clientes
+        Queue<String> colaClientes = new LinkedList<>();
+        colaClientes.add("Maria");
+        colaClientes.add("Pablo");
+        colaClientes.add("Rebeca");
+        colaClientes.add("Pedro");
+        
+        // Titulo
+        System.out.println("\nLista de pedidos pendientes: \n");
+        
+    //Ciclo while para atender a los clientes 
+    while (!colaClientes.isEmpty()) {
+      // Crear una copia de la cola de clientes
+        Queue<String> copiaColaClientes = new LinkedList<>(colaClientes);
+
+        System.out.println("\nAtendiendo a: " + copiaColaClientes.peek());
+        // Se imprime la informacion del ciclo de vida de un pedido.
+        System.out.println("El cliente está seleccionando su orden");
+        System.out.println("Registrando orden");
+        System.out.println("El cliente realizó el pago.");
+
+        // Elimina el cliente de la cola original
+        String clienteAtendido = colaClientes.poll();
+        System.out.println(clienteAtendido + " ha sido atendido.\n");
+
+        // Fila actual despues de atender al cliente que estaba de primer lugar en la lista.
+        System.out.println("Fila actual:\n ");
+        for (String cliente : colaClientes) {
+            System.out.println(cliente);
+        }
+        }
+        // Sale del while para imprimir la pila de empleados.
         System.out.println("Empleados \n");
         PilaEmpleados pila = new PilaEmpleados();
 
@@ -36,5 +76,7 @@ public class Proyecto_estructura_datos {
         pila.apilar(new Empleado(1, "Luis Calderon", "Cajero"));
         System.out.println(pila.imprimirPila());
         
+    
+    
     }
 }
